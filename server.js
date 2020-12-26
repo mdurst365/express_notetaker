f41a7221e8e874e1932c4f92f2f49e2caaadd5c5
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-26 14:29:51 -0500

    added server.js and dependencies

---
+++ server.js
@@ -0,0 +1,10 @@
+//What dependencies will be needed?
+
+//need express to interact with the front end
+const express = require("express");
+//need path for filename paths
+const path = require("path");
+//need fs to read and write files
+const fs = require("fs");
+
+
