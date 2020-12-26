fd52ef8b6c13aeb097924188b4b6d714caafa139
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-26 15:43:54 -0500

    adding notesData

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-//What dependencies will be needed?
+  //What dependencies will be needed?
 
-//need express to interact with the front end
+// need express to interact with the front end
 const express = require("express");
-//need path for filename paths
+// need path for filename paths
 const path = require("path");
-//need fs to read and write files
+// need fs to read and write to files
 const fs = require("fs");
 
+// creating an express server
+const app = express();
+// get the port to listen
+const PORT = process.env.PORT || 7000;
 
+//  Initialize notesData
+
+let notesData = [];
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
