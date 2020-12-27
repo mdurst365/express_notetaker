7f43ca9bc5dd55d26f4202c525475233c45e602e
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-26 20:13:09 -0500

    added API call, return data as JSON.parse array

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-  //What dependencies will be needed?
+//What dependencies will be needed?
 
 // need express to interact with the front end
 const express = require("express");
@@ -20,3 +20,25 @@ let notesData = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Develop/public")));
+
+
+// routes
+
+// api call response for all the notes, and sends the results to the browser as an array of object
+
+app.get("/api/notes", function(err, res) {
+  try {
+    // reads the notes from json file
+    notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+    console.log("Still listening, and I read the notes");
+    // JSON.parse to convert notesData into an array of objects
+    notesData = JSON.parse(notesData);
+
+    // handle errors
+  } catch (err) {
+    console.log("\n error (in app.get.catch):");
+    console.log(err);
+  }
+  //   send objects to the browser
+  res.json(notesData);
+});
